3f079fd437a1b1ae5b13c91ca912a036fa6ff714
sarfaraz45 <sarfaraz45ali@gmail.com>
2020-09-09 15:28:39 +0500

    Ad Js Assignment

---
+++ index.js
@@ -0,0 +1,261 @@
+//Task 1
+// let userName = prompt('Enter your name');
+// let male = "Mr. "
+
+
+// console.log(`Hello!!! ${male} ${userName} `)
+
+//----------Task1 Completed-----------
+
+// Task2
+// var name = prompt("") 
+// console.log(name.length)
+// --------Task2 Completed-------------
+
+// //Task 3 ------------
+// var name = prompt("Enter Name")
+// console.log(name.trim())
+
+// //----------Task3 Completed--------
+
+//Taks4 
+
+// 1. Correct
+// 2. Correct
+// 3. Wrong
+// 4. Wrong
+
+//----------------Task4 Completed -------------
+
+//Task5
+
+// var a = 2 + 6 * 8 - 9 + (2/3) + 9  
+
+// console.log(parseInt(a))
+
+//--------------Task5 Completed--------------
+
+//Task 6
+
+// var product1 = 200 * 3;
+// var product2 = 300; 
+// var discount = 20; 
+// var total = (product1 + product2) / discount
+
+// console.log(`OutPut of the Program Will be =  ${total}`)
+
+// ----------------Taks6 Completed----------------
+
+
+//Task 7
+//  var phoneNumber = "8888"
+// var NIC = 45200-33444444-99
+//  var name = "afaque"
+
+//  if(typeof phoneNumber == 'number'){
+//     console.log(`Phone Number is not a number `)}
+//     else{
+//        console.log(`Data Type of Phone Number should be a number`)
+//    }
+
+
+//    if(typeof NIC == 'string'){
+//        console.log("NIC is string")
+//    } else{
+//        console.log("Data type of NIC should be string")
+//    }
+ 
+//         console.log(`Hello Mr. ${name}` )
+
+// ${typeof(phoneNumber)}
+// ${typeof(NIC)}
+// ${typeof(name)}`)
+
+//Task 7 Done
+
+
+//TASK 8 
+// l
+
+//tAKS 8 cOMPLETED
+
+
+//Task 9 ---------
+
+
+// var product1 = 500 * 3;
+// var product2 = 600;
+// var discount = 20;
+// var total = (product1 + product2) / discount
+// var budget = 100
+
+// if (total < budget) {
+//     console.log("You did not exceed the budget")
+// } else {
+//     console.log("You are not allowed to spend this much money")
+// }
+
+
+//OutPut =    You are not allowed to spend this much money
+
+
+//Taks 9 Completed
+
+//Task 10
+
+// var SAT = 200;
+// var avg = 150;
+// var GPA = 3;
+// var sport = "snooker"
+
+// if (SAT > avg || GPA > 2.5 || sport === "football") {
+//     console.log("You're an awesome student");
+// }
+// else {
+//     console.log("You are not eligible");
+// }
+
+
+//Output = You're an awesome student
+//Task 10 Completed
+
+
+
+//var myMarks = [30,45,68,32,45];
+
+// var myMarks = [33,2,2,4,55];
+// var criteria = 30; 
+// var bonusPoints = 5;
+
+// var newMarks = myMarks.map(function(value){
+//     value=value+5
+//     return value
+//     console.log(value)
+// })
+
+//     if(newMarks<30){
+//             console.log("Student is Fail")
+//      }else{
+//               console.log("Student is Pass")
+//          }
+
+
+// console.log(myMarks)
+// console.log(newMarks)
+
+
+
+
+// console.log(myMarks[0]+5)
+
+// for(let i = 0; i<myMarks.length; i++){
+//     var newMarks = myMarks[i]+5
+//     console.log(newMarks)
+
+// if(newMarks<30){
+//      console.log("Student is Fail")
+//  }else{
+//      console.log("Student is Pass")
+//  }
+
+
+
+//Task 12 
+
+var array=[1,2,3,4,5];
+//  console.log(array.splice(2));
+
+// // OutPut = [3,4,5]
+ 
+// console.log(array);
+
+//OutPut = [1, 2]
+ 
+// var array2=[6,7,8,9,0];
+// // console.log(array2.splice(2,1));
+
+// // //OutPut = 8
+
+ 
+// // console.log(array2.splice(2,0));
+
+// //OutPut = []
+ 
+// console.log(array2);
+
+//outPut = [6,7,8,9,0]
+
+
+// function sayHello(m,r){
+//     return(
+//         console.log(sayHello("d","dd"))
+//     )
+// }
+// var a = "hero"
+
+// function display(name){
+    
+//     console.log(name)
+
+//     result = name.toUpperCase()
+//     console.log(result)
+// }
+// display("faraz")
+
+
+// const arroFunction = (newName)=>{
+//     console.log(newName)
+//     result = newName.toUpperCase()
+//    // console.log(result)
+// }
+
+// arroFunction("pakistan")
+
+// console.log(arroFunction)
+// console.log(result)
+
+//Taks 13 Complted
+
+
+//const arr = [1,2,3,5,66,33]
+/*var ascendingOrder = arr.sort((a, b) => a - b)
+
+const arr2 = [1,2,3,5,66,33]
+var descendingOrder = arr2.sort((a,b) => b - a)
+console.log(ascendingOrder)
+console.log(descendingOrder)
+
+//Taks 13 Complted
+*/
+
+//Task 15
+// function sumAll(a) {
+//  if(typeof(a)=="string"){
+//      console.log("Welcome " + a)
+//  } else if(typeof(a)=="number"){
+//      console.log(Math.pow(a,2))
+//  }
+
+//   return a;
+// }
+
+// // console.log(sumAll("Sarfaraz"))
+// sumAll(9)
+
+//Taks 15 Completed
+
+
+//Task 16
+// function rounOff(num){
+// var roundedNumber =    Math.round(num)
+   
+//    return roundedNumber
+   
+// }
+// console.log(rounOff(4.55))
+
+//Task 16 Completed
+
+
+
+
